d9b990de9273a4b430829d96d20b8e2be45f6a86
Papacita <ignat.700@mail.ru>
2024-10-29 14:29:05 +0300

    add: models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,55 @@
 package models
+
+import "time"
+
+type (
+	BaseModel struct {
+		CreatedAt time.Time `json:"created_at"`
+		Active bool 		`json:"active"`
+	}
+
+	Tag struct {
+		Id int64 		`json:"id"`
+		Name string 	`json:"name"`
+	}
+
+	City struct {
+		Id int64 		`json:"id"`
+		Name string 	`json:"name"`
+	}
+
+	Hotel struct {
+		Id int64 		`json:"id"`
+		Name string 	`json:"name"`
+		Desc string 	`json:"desc"`
+		City_id int64 	`json:"city_id"`
+	}
+
+	RoomCategory struct {
+		Id int64 		`json:"id"`
+		Name string 	`json:"name"`
+		Price float64 	`json:"price"`
+		Capacity int64 	`json:"capacity"`
+		Desc string 	`json:"desc"`
+		Size int64 		`json:"size"`
+		Hotel_id int64 	`json:"hotel_id"`
+	}
+
+	Room struct {
+		Id int64 			`json:"id"`
+		Name string 		`json:"name"`
+		Number string 		`json:"number"`
+		Category_id int64 	`json:"category_id"`
+	}
+
+	Booking struct {
+		Id int64 			`json:"id"`
+		EntryDate time.Time `json:"entry_date"`
+		LeaveDate time.Time `json:"leave_date"`
+		Price float64 		`json:"price"`
+		Status string 		`json:"status"`
+		GuestsCount int64 	`json:"guests_count"`
+		User_id int64 		`json:"user_id"`
+		Room_id int64 		`json:"room_id"`
+	}
+)
